388805adf1554fa64c5d52ba77b9e30a8c3e550e
antoinjose2010 <ajgorvenador22@gmail.com>
2025-08-20 21:35:17 -0300

    tarefa de 1 a 4

---
+++ tarefa.py
@@ -0,0 +1,20 @@
+#1
+
+# vogal="A, E, I, O, U, a, e, i, o, u"
+# contador=0
+# palavra=input("Digite uma palavra e saiba quantas vogais ele tem: ")
+# for caractere in palavra :
+#     if caractere in vogal :
+#         contador+=1
+# print(f"a palavra {palavra} tem {contador} vogais")
+
+#2
+
+# nota1=float(input("Digite a sua nota: "))
+# nota2=float(input("Digite a sua nota: "))
+# nota3=float(input("Digite a sua nota: "))
+# media=(nota1+nota2+nota3) / 3
+# if media >= 6 :
+#     print(f"sua media foi {media}: aprovado")
+# else :
+#     print(f"sua media foi {media}: reprovado")
